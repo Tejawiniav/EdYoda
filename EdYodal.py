c0cd5f2931f3790b654b3360229aedca2e0e2410
Tejaswini <58761961+Tejawiniav@users.noreply.github.com>
2020-02-05 10:15:17 +0530

    Update EdYodal.py

--- EdYodal.py
+++ EdYodal.py
@@ -3,8 +3,8 @@ import mysql.connector
 mydb = mysql.connector.connect(host="localhost", user="root", passwd="root", database="EdYoda")
 
 mycursor = mydb.cursor()
-#Let the Young chap be: bbb
-mycursor.execute("SELECT Ename FROM Elder WHERE Elder.Eid in(SELECT Eid FROM YCaresE WHERE Yid in (SELECT Yid FROM Youth WHERE Yname='bbb'))");
+#Let the Young chap be: ccc
+mycursor.execute("SELECT Ename FROM Elder WHERE Elder.Eid in(SELECT Eid FROM YCaresE WHERE Yid in (SELECT Yid FROM Youth WHERE Yname='ccc'))");
 
 for i in mycursor:
     print(i)
